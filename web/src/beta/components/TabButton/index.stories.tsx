8480f49e07858c472ced453753fee5937e2b5fbd
JAESUNG_PARK <131335554+FabPARKJAESUNG@users.noreply.github.com>
2023-05-30 12:56:39 +0900

    #PASONA_PLATEAU-19

---
+++ web/src/beta/components/TabButton/index.stories.tsx
@@ -0,0 +1,21 @@
+import { Meta } from "@storybook/react";
+
+import TabButton from ".";
+
+export default {
+  title: "beta/components/TabButton",
+  component: TabButton,
+} as Meta;
+
+export const Default = () => (
+  <TabButton label={"Editor"} onClick={() => {}} selected={true} width={"67px"} />
+);
+export const Second = () => (
+  <TabButton label={"Story"} onClick={() => {}} selected={false} width={"62px"} />
+);
+export const Third = () => (
+  <TabButton label={"Widgets"} onClick={() => {}} selected={false} width={"82px"} />
+);
+export const Fourth = () => (
+  <TabButton label={"Preview"} onClick={() => {}} selected={false} width={"81px"} />
+);
---
+++ web/src/beta/components/TabButton/index.tsx
@@ -0,0 +1,66 @@
+// TabButton.tsx
+import type { FC } from "react";
+
+import { styled } from "@reearth/services/theme";
+
+// 以下のようなPropsでお願いします
+export type Props = {
+  label: string;
+  selected: boolean;
+  width: string;
+  onClick: () => void;
+};
+
+const TabButton: FC<Props> = ({ label, selected, onClick, width }) => {
+  return (
+    <StyledButton onClick={onClick} width={width}>
+      <StyledText selected={selected}>{label}</StyledText>
+    </StyledButton>
+  );
+};
+const StyledButton = styled.button<{
+  width: string;
+}>`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 8px 12px;
+  gap: 10px;
+
+  position: absolute;
+  width: ${({ width }) => width};
+  height: 35px;
+  left: 0px;
+  top: 0px;
+
+  background: #232226;
+  border-radius: 4px;
+  :hover {
+    background: #38383b;
+  }
+`;
+
+type TextProps = {
+  selected: boolean;
+};
+
+const StyledText = styled.text<TextProps>`
+  width: 100%;
+  height: 19px;
+
+  font-family: "Noto Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 14px;
+  line-height: 19px;
+
+  text-align: center;
+
+  color: ${({ selected }) => (selected ? "#c7c5c5" : "#4A4A4A")};
+
+  flex: none;
+  order: 0;
+  flex-grow: 0;
+`;
+
+export default TabButton;
