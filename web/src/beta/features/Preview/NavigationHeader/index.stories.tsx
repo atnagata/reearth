51370c38ae9a6dfec289c0a6981df7b3a7024f4d
JAESUNG_PARK <131335554+FabPARKJAESUNG@users.noreply.github.com>
2023-06-21 14:06:45 +0900

    Chore(web): add story [NavigationHeader]

---
+++ web/src/beta/features/Preview/NavigationHeader/index.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import NavigationHeader from ".";
+
+export default {
+  component: NavigationHeader,
+} as Meta;
+
+type Story = StoryObj<typeof NavigationHeader>;
+
+export const Default: Story = {};
